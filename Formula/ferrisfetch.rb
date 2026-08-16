9f685aad94fbc4d7736bdb588309e6fc284dddec
Kushagra Kumar <kkushagra86@gmail.com>
2026-08-16 23:51:34 +0530

    Add ferrisfetch formula

---
+++ Formula/ferrisfetch.rb
@@ -0,0 +1,20 @@
+class Ferrisfetch < Formula
+  desc "Fast, lightweight Linux system information fetch CLI written in Rust"
+  homepage "https://github.com/Kk376/ferrisfetch"
+  url "https://github.com/Kk376/ferrisfetch/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "6e2fbf052988cb945c436087f42dcf9ebd8885d3cd19638b58221e7d41eca9df"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+    bash_completion.install "completions/ferrisfetch.bash" => "ferrisfetch"
+    zsh_completion.install "completions/_ferrisfetch"
+    fish_completion.install "completions/ferrisfetch.fish"
+  end
+
+  test do
+    assert_match "ferrisfetch", shell_output("#{bin}/ferrisfetch --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,20 @@
+# homebrew-tap
+
+Homebrew tap for FerrisFetch and tools.
+
+## Installation
+
+```bash
+brew tap Kk376/tap
+brew install ferrisfetch
+```
+
+Or install directly:
+
+```bash
+brew install Kk376/tap/ferrisfetch
+```
+
+## Formulas
+
+- **ferrisfetch**: Fast, lightweight Linux system information fetch CLI written in Rust
