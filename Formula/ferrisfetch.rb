140c30d2f9bbc57cb6dc20edb03a6939822c7cd3
Kushagra Kumar <kkushagra86@gmail.com>
2026-08-22 21:28:25 +0530

    feat(formula): update v0.9.0 archive sha256

--- Formula/ferrisfetch.rb
+++ Formula/ferrisfetch.rb
@@ -2,7 +2,7 @@ class Ferrisfetch < Formula
   desc "Fast, lightweight Linux system information fetch CLI written in Rust"
   homepage "https://github.com/kk376/ferrisfetch"
   url "https://github.com/kk376/ferrisfetch/archive/refs/tags/v0.9.0.tar.gz"
-  sha256 "d1fd0d20100dc13dbf76f1ea1bc8dd8b2418d749d4d8a19e2effc47b6f216fab"
+  sha256 "9fc28bad27147ff5d4ed147a829425a4add5cb029a5b318ed10972ae58567c4b"
   license "MIT"
 
   depends_on "rust" => :build
