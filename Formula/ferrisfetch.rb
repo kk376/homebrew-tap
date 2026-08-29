dfa387ce3ad886031c5828c5ef026bb98df65066
Kushagra Kumar <kkushagra86@gmail.com>
2026-08-29 06:20:01 +0530

    feat: bump ferrisfetch to v0.11.0

--- Formula/ferrisfetch.rb
+++ Formula/ferrisfetch.rb
@@ -1,8 +1,8 @@
 class Ferrisfetch < Formula
   desc "Fast, lightweight Linux system information fetch CLI written in Rust"
   homepage "https://github.com/kk376/ferrisfetch"
-  url "https://github.com/kk376/ferrisfetch/archive/refs/tags/v0.10.2.tar.gz"
-  sha256 "a54819fd8e72d68de9f2231ccecc9db1d35367ad61076bd33608d0150d39d9cc"
+  url "https://github.com/kk376/ferrisfetch/archive/refs/tags/v0.11.0.tar.gz"
+  sha256 "3f8a5ce13d9f12a80dac375911fed5ec156129be819c437d543668e3d1a748a1"
   license "MIT"
 
   depends_on "rust" => :build
